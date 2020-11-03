8eced5d54402b73bd3661725de0fda711fecfba8
alexw0610 <alex@mailbag.at>
2020-11-03 12:26:51 +0100

    implemented training function

--- src/ANN.java
+++ src/ANN.java
@@ -45,12 +45,13 @@ public class ANN {
 
     }
 
-    public void train(float[][] testCases, float[] testSolutions,float margin, int iterations){
+    public void train(float[][] testCases, float[][] testSolutions,float margin, int iterations){
+
         float errorSum = margin+1;
         int iterCount = 0;
         while(iterCount < iterations){
             int picker = (int)Math.floor(Math.random()*(testCases.length));
-            System.out.println("Test case "+testCases[picker][0]+" "+testCases[picker][1]+" "+testSolutions[picker]);
+            System.out.println("Test case "+testCases[picker][0]+" "+testCases[picker][1]+" "+testSolutions[picker][0]);
             errorSum = Math.abs(train(testCases[picker],testSolutions[picker]));
 
             iterCount++;
@@ -71,51 +72,45 @@ public class ANN {
         System.out.println("Training finished after "+iterCount+" iterations.");
         System.out.println("Error sum: "+errorSum);
         System.out.println("All weights updated!");
+        
     }
 
-    private float train(float[] input, float solution){
+    private float train(float[] input, float[] solution){
 
-        // new weight = weight + LR * OutError * output(neuron i (before weight))* sigDeriv(neuron i+1 (after weight))
-
-        float[] result = predict(input.clone());
-        float[] error = subtract(result,solution);
-        float errorSingle = sum(error)/error.length;
-        //float errorSingle = error[0];
-        float errorSum = (float)((1/error.length)*Math.pow(sum(error),2));
-        System.out.println("Error: "+errorSingle);
-        float[] derivSigmoidOutput = derivSigmoid(new float[]{solution});
-        float[] temp = hidden.clone();
-        float[] derivSigmoidNeurons = derivSigmoid(temp);
-
-
-        // weights HO
-
-        for (int hiddenNeuron = 0; hiddenNeuron < weightsHO.length; hiddenNeuron++) {
-            for(int weight = 0; weight < weightsHO[0].length; weight++){
-                weightsHO[hiddenNeuron][weight] += learningRate * errorSingle * hidden[hiddenNeuron] * derivSigmoidOutput[weight];
+        float[] results = predict(input);
+        float[] derivSigmoid = derivSigmoid(results.clone());
+        float[] derivSigmoidHidden = derivSigmoid(hidden.clone());
+        float[] error = subtract(solution,results);
+        float[] costHidden = new float[hidden.length];
+        for (int i = 0; i < hidden.length; i++) {
+            for (int j = 0; j < results.length; j++) {
+                costHidden[i] += weightsHO[i][j]*(-error[j]);
             }
         }
+
 
         for (int i = 0; i < biasOutput.length; i++) {
-            biasOutput[i] += learningRate * errorSingle  * derivSigmoidOutput[i];
+            biasOutput[i] -= 1 * derivSigmoid[i]* (-error[i]) * learningRate;
+        }
+        for (int i = 0; i < biasHidden.length; i++) {
+            biasHidden[i] -= 1 * derivSigmoidHidden[i] * costHidden[i] * learningRate;
         }
 
-        // weights IH
 
-        for (int hiddenNeuron = 0; hiddenNeuron < weightsIH.length; hiddenNeuron++) {
-            for(int weight = 0; weight < weightsIH[0].length; weight++){
-                weightsIH[hiddenNeuron][weight] += learningRate * errorSingle * input[weight] * derivSigmoidNeurons[weight];
+
+        for (int i = 0; i < weightsHO.length; i++) {
+            for (int z = 0; z < weightsHO[0].length; z++) {
+                weightsHO[i][z] -= (hidden[i] * derivSigmoid[z] * (-error[z]) * learningRate);
+            }
+        }
+        for (int i = 0; i < weightsIH[0].length; i++) {
+            for (int z = 0; z < weightsIH.length; z++) {
+                weightsIH[z][i] -= (input[i] * costHidden[z] * derivSigmoidHidden[z] * learningRate);
             }
         }
 
-        for (int i = 0; i < biasHidden.length; i++) {
-            biasHidden[i] += learningRate * errorSingle * derivSigmoidNeurons[i];
-        }
 
-
-
-       return errorSum;
-
+        return 1;
     }
 
     public float[] predict(float[] input){
@@ -171,13 +166,6 @@ public class ANN {
         return arg1;
     }
 
-    private static float sum(float[] arg1){
-        float temp = 0;
-        for(int i = 0; i < arg1.length; i++){
-            temp += arg1[i];
-        }
-        return temp;
-    }
 
     private static float[] subtract(float[] arg1, float[] arg2){
 
@@ -192,116 +180,18 @@ public class ANN {
         return temp;
     }
 
-    private static float[] subtract(float[] arg1, float arg2){
 
-
-        float[] temp = new float[arg1.length];
-        for(int i = 0; i < arg1.length; i++){
-            temp[i] = arg2-arg1[i];
-        }
-        return temp;
-    }
-
-    private static float[] add(float[] arg1, float[] arg2){
-
-        if(arg1.length != arg2.length){
-            System.err.println("Addition error. Arrays are not of equal length!");
-        }
-
-        float[] temp = new float[arg1.length];
-        for(int i = 0; i < arg1.length; i++){
-            temp[i] = arg1[i]+arg2[i];
-        }
-        return temp;
-    }
-
-    private static float[][] add(float[][] arg1, float[] arg2){
-
-        float[][] temp = new float[arg1.length][arg1[0].length];
-        for(int i = 0; i < arg1.length; i++){
-            temp[i] = add(arg1[i],arg2);
-        }
-        return temp;
-    }
-
-    private static float[][] transpose(float[] arg1){
-        float[][] temp = new float[0][arg1.length];
-
-        for(int i = 0; i < arg1.length; i++){
-            temp[0][i] = arg1[i];
-        }
-        return temp;
-    }
-
-    private static float[][] transpose(float[][] arg1){
-        float[][] temp = new float[arg1[0].length][arg1.length];
-
-        for(int row = 0; row < arg1.length; row++){
-            for(int col = 0; col < arg1[0].length; col++){
-                temp[col][row] = arg1[row][col];
-            }
-        }
-        return temp;
-    }
-
-    private static float multiply(float[] arg1, float[] arg2){
-
-        if(arg1.length != arg2.length){
-            System.err.println("Multiplication error. Arrays are not of equal length!");
-        }
-
-        float temp = 0;
-
-        for (int i = 0; i < arg1.length; i++) {
-            temp += arg1[i]*arg2[i];
-        }
-
-        return temp;
-    }
-
-    private static float[] multiply(float[] arg1, float[][] arg2){
-
-        if(arg1.length != arg2[0].length){
-            System.err.println("Multiplication error. Rows and cols dont match!");
-        }
-
-        float[] temp = new float[arg2[0].length];
-        for (int i = 0; i < arg1.length; i++) {
-            for (int j = 0; j < arg2.length; j++) {
-                temp[i] += arg2[j][i] * arg1[j];
-            }
-
-        }
-        return temp;
-    }
-
-    private static float[] multiply(float[] arg1, float arg2){
-
-        float[] temp = new float[arg1.length];
-        for (int i = 0; i < arg1.length; i++) {
-            temp[i] = arg1[i]*arg2;
-        }
-        return temp;
-    }
-
-    private static float[][] multiply(float[][] arg1, float arg2){
-
-        float[][] temp = new float[arg1.length][arg1[0].length];
-        for (int row = 0; row < arg1.length; row++) {
-            for (int col = 0; col < arg1[0].length; col++) {
-                temp[row][col] = arg1[row][col]*arg2;
-            }
-        }
-        return temp;
-    }
 
     public static void main(String[] args) {
 
         ANN ann = new ANN(2,3,1);
-        float[][] trainData = {{0,0},{1,0},{0,1},{1,1}};
-        float[] trainSolutions = {0,1,1,0};
+        float[][] trainData = {{0,0},
+                {1,0},
+                {0,1},
+                {1,1}};
+        float[][] trainSolutions = {{0},{1},{1},{0}};
 
-        ann.train(trainData,trainSolutions,0.001f,50000);
+        ann.train(trainData,trainSolutions,0.001f,500000);
 
         float[] inputs = new float[]{0,0};
         float[] result = ann.predict(inputs);
