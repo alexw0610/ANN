1c3252d4ef2f89481d2e95428e594285cfacf2b6
alexw0610 <alex@mailbag.at>
2020-11-09 19:05:10 +0100

    added training routing for mnist Digit images

--- src/MnistDigit.java
+++ src/MnistDigit.java
@@ -7,10 +7,12 @@ public class MnistDigit {
         The location of the unziped binary files from the mnist database. The first argument is the image data the second one the image labels.
          */
         MnistFileReader mnistObject = new MnistFileReader("./resources/train-images.idx3-ubyte","./resources/train-labels.idx1-ubyte");
-        ANN network = new ANN(mnistObject.imgSize,mnistObject.imgSize,30,10);
+        ANN network = new ANN(mnistObject.imgSize,20,5,10);
+
+        //network.loadWeightsFromDisk("./ANN2.model");
 
         double timeSum = 0;
-        for(int trainPatch = 0; trainPatch < 50; trainPatch++){
+        for(int trainPatch = 0; trainPatch < 600; trainPatch++){
 
             double startTime = System.currentTimeMillis();
 
@@ -21,22 +23,27 @@ public class MnistDigit {
             Note that the amount of samples you chose here should be somewhat fitting for the epoch amount.
              */
             for (int i = 0; i < 100; i++) {
-                MnistFileReader.TrainingSet set = mnistObject.getTrainingSet(i+trainPatch*10);
+                MnistFileReader.TrainingSet set = mnistObject.getTrainingSet(i+trainPatch*100);
                 input[i] = set.imgData;
                 solutions[i] = set.imgLabel;
             }
 
 
-            network.train(input,solutions,2000,0.01f,1);
-
+            network.train(input,solutions,1000,0.01f,1);
+            /*
+            if (trainPatch%5==0){
+                network.persistWeightsToDisk("./ANN2.model");
+            }
+            */
             double endTime = System.currentTimeMillis();
             double timeTaken = endTime-startTime;
             timeSum += timeTaken;
-            double timeToFinish = ((timeSum/(trainPatch+1))*(50-trainPatch+1));
-            int minutesToFinish = (((int)timeToFinish)/1000)/60;
+            double timeToFinish = ((timeSum/(trainPatch+1))*(600-trainPatch+1));
+            int hoursToFinish = (((int)timeToFinish)/1000)/60/60;
+            int minutesToFinish = ((((int)timeToFinish)/1000)/60)%60;
             int secondsToFinish = (((int)timeToFinish)/1000)%60;
 
-            System.out.println("Training cycle ["+(trainPatch+1)+"] finished.    Estimated time remaining: "+minutesToFinish+"m "+secondsToFinish+"s");
+            System.out.println("Training cycle ["+(trainPatch+1)+"] finished.\t Estimated time remaining: "+hoursToFinish+"h "+minutesToFinish+"m "+secondsToFinish+"s");
         }
 
 
@@ -44,16 +51,37 @@ public class MnistDigit {
         /*
         Test the trained neural network on a sample from the mnist database.
          */
-        MnistFileReader.TrainingSet test = mnistObject.getTrainingSet(40);
-        float[] result = network.predict(test.imgData);
-        System.out.println("Test Case 1:");
-        System.out.println("Is: "+mnistObject.binaryToLabel(test.imgLabel));
-        mnistObject.printImgToConsole(test.imgData);
-        System.out.println("Is : Predicted");
-        for (int index = 0; index < 10; index++) {
-            System.out.println( test.imgLabel[index]+" : "+Math.round(result[index])+" "+result[index]);
+        int correct = 0;
+        int count = 0;
+        for (int testcase = 50; testcase < 250; testcase++) {
+            MnistFileReader.TrainingSet test = mnistObject.getTrainingSet(testcase);
+            float[] result = network.predict(test.imgData);
+            System.out.println("Test Case "+testcase+":");
+            //System.out.println("Is: "+mnistObject.binaryToLabel(test.imgLabel));
+            //mnistObject.printImgToConsole(test.imgData);
+            System.out.println("Is ["+mnistObject.binaryToLabel(test.imgLabel)+"] Predicted ["+getMax(result)+"]");
+            if(mnistObject.binaryToLabel(test.imgLabel) == getMax(result)){
+                correct++;
+            }
+            System.out.println("Is % : Predicted %");
+            for (int index = 0; index < 10; index++) {
+                System.out.println( test.imgLabel[index]+" : "+result[index]);
+            }
+            count++;
         }
 
+        System.out.println("Prediction accuracy: "+(((float)correct/(float)count)*100)+"%");
+
+
+
+    }
+
+    private static int getMax(float[] arg){
+        int maxIndex = 0;
+        for(int i = 0; i < arg.length;i++){
+            maxIndex = arg[i] > arg[maxIndex] ? i : maxIndex;
+        }
+        return maxIndex;
     }
 
 }
